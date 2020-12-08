176e73ca05dea216a01b55396fc253f2b6c431e0
rojeheki <75195617+rojeheki@users.noreply.github.com>
2020-12-08 14:03:48 +0100

    Update Optimizations.R

--- DeatonLaroque/Optimizations.R
+++ DeatonLaroque/Optimizations.R
@@ -3,15 +3,15 @@ funIID = function (data, par) {
 }
 
 funAR = function (data, par) {
-  return (- calculateEverything(data[[1]], par[4], par[1:3]))
+  return (- calculateEverything(data[[1]], 1-exp(par[4]), par[1:3]))
 }
 
-initEverything(com$cocoa)
+
 initEverything(com$coffee)
 
-system.time(calculateEverything(com$cocoa,0,c(3,0.693,-1.772)))
+system.time(calculateEverything(com$coffee,0,c(3,0.693,-1.772)))
 
-optIID = optim(par = c(3, 0.693, -1.772), fn = funIID, data = c(com$coffee, 0), control=list(maxit=100))
+optIID = optim(par = c(2, 1, 0.6), fn = funIID, data = c(com$coffee, 0), control=list(maxit=200))
 
-optAR = optim(par = c(3, 0.693, -1.772, 0.7), fn = funIID, data = c(com$cocoa))
+optAR = optim(par = c(3, 0.693, -1.772, -1.2), fn = funAR, data = c(com$coffee), control=list(maxit=200))
 
