f03ed414f442589dea0bf72cacd023a191911abc
rojeheki <75195617+rojeheki@users.noreply.github.com>
2020-12-08 17:16:23 +0100

    Update Optimizations.R

--- DeatonLaroque/Optimizations.R
+++ DeatonLaroque/Optimizations.R
@@ -1,8 +1,11 @@
+# Run DeatonLaroqueCore.R before this file
+
+
 # Setup sample data and benchmark time requirement
 
 initEverything(com$coffee)
 
-system.time(calculateEverything(com$coffee))
+system.time(for(i in 1:10){calculateEverything(com$coffee, output=FALSE)})
 
 # Using transformations so that the optimization can be unbounded
 
@@ -29,7 +32,12 @@ convertFromPar = function(par){
   return (c(a,b,delta,rho))
 }
 
-convertToPar = function(a,b,delta,rho){
+convertToParIID = function(a,b,delta){
+  par = c(a, log(-b+0.01), log(delta+0.05))
+  return (par)
+}
+
+convertToParAR = function(a,b,delta,rho){
   par = c(a, log(-b+0.01), log(delta+0.05), log(0.99-rho))
   return (par)
 }
@@ -37,18 +45,17 @@ convertToPar = function(a,b,delta,rho){
 
 # Nelder-Mead
 
-optIID = optim(par = convertToPar(0.2, -0.15, 0.12, 0), fn = funIID, data = c(com$coffee), control=list(maxit=200, alpha=1.2, beta=0.9))
+optIID = optim(par = convertToParIID(0.2, -0.15, 0.12), fn = funIID, data = c(com$coffee), control=list(maxit=200, alpha=1.2, beta=0.9))
 convertFromPar(optIID$par)
 
 
-optAR = optim(par = convertToPar(0.2, -0.15, 0.12, 0.7), fn = funAR, data = c(com$coffee), control=list(maxit=200))
+optAR = optim(par = convertToParAR(0.2, -0.15, 0.12, 0.7), fn = funAR, data = c(com$coffee), control=list(maxit=200))
+convertFromPar(optAR$par)
 
 
-# BFGS (tries to set parameters to Inf or -Inf)
+# BFGS - tries to set parameters to extremely large values - could not get it to work
 
-optIID = optim(par = convertToPar(0.2, -0.15, 0.12, 0), fn = funIID, data = c(com$coffee), method = "BFGS", control=list(maxit=200))
-
-
+optIID = optim(par = convertToParAR(0.2, -0.15, 0.12, 0), fn = funIID, data = c(com$coffee), method = "BFGS", control=list(maxit=200))
 
 
 # Using bounded optimization
@@ -63,5 +70,5 @@ funAR = function (data, par) {
   return (- calculateEverything(data[[1]], par[1], par[2], par[3], par[4]))
 }
 
-optIID = optim(par = c(0.2, -0.15, 0.12), lower = c(0.01,-1,0), upper = c(1,0,1), fn = funIID, data = c(com$coffee), method="L-BFGS-B", control=list(maxit=200))
+optIID = optim(par = c(0.2, -0.15, 0.12), lower = c(0.01,-1,0.01), upper = c(1,0,1), fn = funIID, data = c(com$coffee), method="L-BFGS-B", control=list(maxit=200))
 
